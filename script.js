206fbee2a52e8c83160b7adf8f15b36e4a11e9b4
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2022-02-10 19:31:08 -0300

    Basic operations with two integers working

--- script.js
+++ script.js
@@ -17,11 +17,44 @@ function operate(operator, num1, num2){
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-        case 'x':
         case 'X':
-        case '*':
             return multiply(num1, num2);
         case '/':
             return divide(num1, num2);
     }
 }
+
+let displayContent = document.querySelector('.display-text');
+let numberButtons = document.querySelectorAll('.number-buttons');
+let operationButtons = document.querySelectorAll('.operation-buttons');
+let clearButton = document.querySelector('.clear-button');
+let backspace = document.querySelector('.backspace');
+let equalButton = document.querySelector('.equal-button');
+
+let operation,num1,num2,eternal1;
+
+numberButtons.forEach(button => button.addEventListener("click", (event)=>{
+    displayContent.textContent += event.target.textContent;
+    num1 += event.target.textContent;
+  }))
+
+operationButtons.forEach(button => button.addEventListener('click', (event)=>{
+    num1 = displayContent.textContent;
+    const cu = num1;
+    num1 = "";
+    parseInt(eternal1 = cu);
+    displayContent.textContent += event.target.textContent;
+    operation = `${event.target.textContent}`;
+} ))
+
+clearButton.addEventListener('click', ()=>{
+    displayContent.textContent = "";
+    operation = "";
+    num1 = "";
+    num2 = "";
+})
+
+equalButton.addEventListener('click', (event)=>{
+    displayContent.textContent += event.target.textContent;
+    displayContent.textContent = operate(operation, +eternal1, +num1);
+})
