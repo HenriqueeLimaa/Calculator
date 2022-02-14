30f232a87ac084ce65e6728d84293ee8d5fa34ea
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2022-02-14 20:35:10 -0300

    Add functionality to the backspace button and decimal button, round results

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function add(num1, num2){
-    return num1 + num2;
+    let result = (num1 + num2);
+    return (Number.isInteger(result)) ? result:result.toFixed(1);
 }
 function subtract(num1, num2){
-    return num1 - num2;
+    let result = num1 - num2;
+    return (Number.isInteger(result)) ? result:result.toFixed(1);
 }
 function multiply(num1, num2){
-    return num1 * num2;
+    let result = num1 * num2;
+    return (Number.isInteger(result)) ? result:result.toFixed(1);
 }
 function divide(num1, num2){
-    return num1 / num2;
+    let result = num1 / num2;
+    return (Number.isInteger(result)) ? result:result.toFixed(1);
 }
 
 function operate(operator, num1, num2){
+    let alright;
     switch (operator) {
         case '+':
+            if(num2 === 1 && alright) {num2-=1, alright = true}
             return add(num1, num2);
         case '-':
+            if(num2 === 1 && alright) {num2-=1, alright = true}
             return subtract(num1, num2);
         case 'X':
             return multiply(num1, num2);
@@ -30,9 +37,10 @@ let operationButtons = document.querySelectorAll('.operation-buttons');
 let clearButton = document.querySelector('.clear-button');
 let backspace = document.querySelector('.backspace');
 let equalButton = document.querySelector('.equal-button');
+let decimalButton = document.querySelector('.decimal-button');
 
 //Store the numbers used as arguments in the operate function
-let operation,num1,eternal1;
+let operation,num1,eternal1,saveMe,lockDecimal;
 
 //Detect when the user click on a number button, makes it appear on the display and stores it on num1
 numberButtons.forEach(button => button.addEventListener("click", (event)=>{
@@ -47,17 +55,23 @@ operationButtons.forEach(button => button.addEventListener('click', (event)=>{
     if(operation){
         eternal1 = operate(operation, +eternal1, +num1);
         operation = "";
+        num1 = 1;
+        saveMe = false;
+        console.log(num1);
         displayContent.textContent = eternal1 + operation;
     }//If the user didnt chose an operation yet, saves the first number in the eternal1 variable, cleans
     //num1 variable so it can be used again as the second value in the operate function, and display the
     //numbers. 
     else{
-        num1 = displayContent.textContent;
-        const holdNum1 = num1;
-        num1 = "";
-        parseInt(eternal1 = holdNum1);
+        if(!saveMe){
+            num1 = displayContent.textContent;
+            parseInt(eternal1 = num1);
+            num1 = "";
+        }
         operation = event.target.textContent;
         displayContent.textContent += operation;
+        saveMe = true;
+        lockDecimal = false;
     }
 } ))
 
@@ -65,19 +79,30 @@ clearButton.addEventListener('click', ()=>{
     displayContent.textContent = "";
     operation = "";
     num1 = "";
+    eternal1 = "";
+    saveMe = false;
+    lockDecimal = false;
 })
 
 backspace.addEventListener('click', ()=>{
-    
+    displayContent.textContent = displayContent.textContent.slice(0,-1);
+    num1 = num1.slice(0,-1);
 })
 
 //Display results once the user click on the = button.
 equalButton.addEventListener('click', ()=>{
-    showResult();
+    displayContent.textContent = operate(operation, +eternal1, +num1);
+    operation = "";
+    eternal1 = "";
+    num1 = "";
+    lockDecimal = false;
 })
 
 // if i do 7x7 and click in the X button again and then change to - it shows a bug
-
-function showResult(){
-    displayContent.textContent = operate(operation, +eternal1, +num1);
-}
+decimalButton.addEventListener('click', (event)=>{
+    if(!lockDecimal){
+        displayContent.textContent += event.target.textContent;
+        num1 = event.target.textContent;
+        lockDecimal = true;
+    }
+})
